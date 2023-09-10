40d64ff8ec18dfef5fd53b332c820d2b5bbe3e14
okinep6 <144177604+okinep6@users.noreply.github.com>
2023-09-10 00:47:46 -0700

    Beginnerproject.js

--- jsProof/Beginnerproject.js
+++ jsProof/Beginnerproject.js
@@ -6,19 +6,26 @@ Assessment Requirements
    you will store it in the variable you created in step 1
 3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
 4. For good measure, getTotalSupply() should return the number of NFT's you have created
+*/
 
+// create a variable to hold your NFT's
 const nftCollection = [];
 
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
 function mintNFT(_name, _level, _colour, _bling) {
   const newNFT = {
     name: _name,
     level: _level,
     colour: _colour
-    bling: _bling
+    chain: _chain
   };
   nftCollection.push(newNFT);
 }
 
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
 function listNFTs() {
   for (const nft of nftCollection) {
     console.log("Name: " + nft.name);
@@ -29,15 +36,15 @@ function listNFTs() {
   }
 }
 
+
+// print the total number of NFTs we have minted to the console
 function getTotalSupply() {
   return nftCollection.length;
 }
 
-mintNFT("NFT 1", "Level 9", "Blue", "ice");
+mintNFT("NFT 1", "Level 9", "Blue", "Ice");
 mintNFT("NFT 2", "Level 10", "Pink", "Silver");
 
+// call your functions below this line
 console.log("List of NFTs:");
 listNFTs();
-
-const totalSupply = getTotalSupply();
-console.log("Total NFTs Minted: " + totalSupply);
